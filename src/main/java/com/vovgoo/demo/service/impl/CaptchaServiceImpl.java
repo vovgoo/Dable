f0173f70e2a1d0ed2629be2391eab29d1177c8a9
Vova <vov.gonchar2017@yandex.ru>
2025-07-27 20:15:37 +0300

    remove rest template, add web client, add webflux dependency

--- src/main/java/com/vovgoo/demo/service/impl/CaptchaServiceImpl.java
+++ src/main/java/com/vovgoo/demo/service/impl/CaptchaServiceImpl.java
@@ -4,45 +4,34 @@ import com.vovgoo.demo.config.properties.CaptchaProperties;
 import com.vovgoo.demo.dtos.captcha.CaptchaResponse;
 import com.vovgoo.demo.service.CaptchaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
 public class CaptchaServiceImpl implements CaptchaService {
 
     private final CaptchaProperties captchaProperties;
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     @Override
     public Boolean verifyCaptcha(String token) {
-        HttpEntity<String> request = buildRequest(token);
-        ResponseEntity<CaptchaResponse> response = sendRequest(request);
-        return parseResponse(response);
-    }
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("secret", captchaProperties.getSecret());
+        body.add("response", token);
 
-    private HttpEntity<String> buildRequest(String token) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        CaptchaResponse captchaResponse = webClient.post()
+                .uri(captchaProperties.getVerifyUrl())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(CaptchaResponse.class)
+                .block();
 
-        String body = buildRequestBody(token);
-        return new HttpEntity<>(body, headers);
-    }
-
-    private String buildRequestBody(String token) {
-        return "secret=" + captchaProperties.getSecret() +
-                "&response=" + token;
-    }
-
-    private ResponseEntity<CaptchaResponse> sendRequest(HttpEntity<String> request) {
-        return restTemplate.postForEntity(captchaProperties.getVerifyUrl(), request, CaptchaResponse.class);
-    }
-
-    private boolean parseResponse(ResponseEntity<CaptchaResponse> response) {
-        return response.getBody().isSuccess();
+        return captchaResponse != null && captchaResponse.isSuccess();
     }
 }
