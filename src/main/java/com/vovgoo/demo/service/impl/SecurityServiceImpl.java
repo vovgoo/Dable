adf39de099482b428760d305405ea02f0f9e2fcc
Vova <vov.gonchar2017@yandex.ru>
2025-07-29 00:18:00 +0300

    refactor user service

--- src/main/java/com/vovgoo/demo/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/vovgoo/demo/service/impl/SecurityServiceImpl.java
@@ -3,19 +3,18 @@ package com.vovgoo.demo.service.impl;
 import com.vovgoo.demo.service.SecurityService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityServiceImpl implements SecurityService {
 
     @Override
-    public String getAuthenticatedUsername() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if(authentication == null || !authentication.isAuthenticated()){
+    public UserDetails getCurrentUserDetails() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated() || auth.getPrincipal() instanceof String) {
             throw new RuntimeException("User not authenticated");
         }
-
-        return authentication.getName();
+        return (UserDetails) auth.getPrincipal();
     }
 }
